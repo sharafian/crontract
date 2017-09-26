c64745785fa5618406488c53e0624df8453da017
Ben Sharafian <benms64@gmail.com>
2017-09-26 11:27:14 -0700

    feat: switch ubuntu to node because it has more things installed

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function runJob (id) {
     'run',
     '--rm', // remove container after task
     '-i', // don't daemonize
-    'ubuntu', // runs bash on ubuntu image
+    'node', // runs bash on node image because it has stuff installed
     '/bin/bash', '-c', jobs[id].task // run task as bash command
   ], { stdio: 'inherit', detached: false, shell: false })
 
